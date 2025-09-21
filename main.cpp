6edffbc2b45691c800628e2a4c490d17893939e7
nanacrackingthesecodes <dianakurochkina22@gmail.com>
2025-09-21 15:46:41 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Laptop.h"
+
+int main() {
+    Laptop obj1("Macbook", "White", 10000);
+    obj1.Print();
+
+    Laptop obj2("Dell", "Black", 5000);
+    obj2.Print();
+
+    Laptop obj3("Asus", "Silver", 8000);
+    obj3.Print();
+
+    return 0;
+}
